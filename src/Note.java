b272c8c6fc80139b324d76f994caa6862e57e01b
VARSHITH DHARIPALLY <25211a05kq@bvrit.ac.in>
2026-07-05 18:54:34 +0530

    Refactor MindGarden to use object-oriented Note model
    - Refactored MindGarden from String[] to Note[]
    - Created a dedicated Note class
    - Implemented constructor and object initialization
    - Added displayNote() for object-based display
    - Applied encapsulation using private fields
    - Added getters and markAsCompleted() method
    - Refactored add, display, and search features to use Note objects
    - Improved project structure by separating Main.java and Note.java

---
+++ src/Note.java
@@ -0,0 +1,60 @@
+public class Note
+{
+        private String title;
+          private String category;
+          private String createdAt;
+          private String content;
+          private boolean completed;
+
+       public Note(String title, String category,String createdAt,String content)
+        {
+            this.title = title;
+            this.category = category;
+            this.createdAt = createdAt;
+            this.content = content;
+            this.completed = false;
+
+        }
+
+
+  public void displayNote()
+    {
+        System.out.println("------------------------------");
+        System.out.println("Title      : " + title);
+        System.out.println("Category   : " + category);
+        System.out.println("Created At : " + createdAt);
+        System.out.println("Content    : " + content);
+        System.out.println("Completed  : " + completed);
+        System.out.println("------------------------------");
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getCategory()
+    {
+        return category;
+    }
+
+    public String getCreatedAt()
+    {
+        return createdAt;
+    }
+
+    public String getContent()
+    {
+        return content;
+    }
+
+    public boolean isCompleted()
+    {
+        return completed;
+    }
+
+    public void markAsCompleted()
+    {
+        completed = true;
+    }
+}
